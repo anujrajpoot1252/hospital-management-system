7dbb9210700f2c04dc1741b40fc8e2f9c0f9068c
anujrajpoot1252 <anujrajpoot1252@gmail.com>
2026-04-05 21:41:54 +0530

    Update and rename patient_db (1).sql to patient_db.sql
    Signed-off-by: anujrajpoot1252 <anujrajpoot1252@gmail.com>

--- patient_db.sql
+++ patient_db.sql
@@ -1,10 +1,3 @@
-
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 CREATE TABLE `patient` (
   `Name` text NOT NULL,
   `Email` varchar(100) NOT NULL,
@@ -15,7 +8,3 @@ CREATE TABLE `patient` (
   `disease` text NOT NULL,
   `medical_history` text NOT NULL,
   `gender` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-COMMIT;
-
-
