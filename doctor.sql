bc24e6e705db11e8054e70ec9ebd3984e01bdc70
khushboobaghel411-maker <khushboobaghel411@gmail.com>
2026-04-19 19:04:11 +0530

    Add files via upload
    Signed-off-by: khushboobaghel411-maker <khushboobaghel411@gmail.com>

---
+++ doctor.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `doctor` (
+  `ID` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `Name` text NOT NULL,
+  `Department` text NOT NULL,
+  `Experience` varchar(50) NOT NULL,
+  `Availability` varchar(50) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Phone` int(10) NOT NULL
+) 
