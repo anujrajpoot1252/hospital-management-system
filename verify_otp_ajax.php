67b9cd1b2833dc03d89d8ebe804e55d3c31ac11e
anujrajpoot1252 <anujrajpoot1252@gmail.com>
2026-05-23 21:48:34 +0530

    Update verify_otp_ajax.php

--- verify_otp_ajax.php
+++ verify_otp_ajax.php
@@ -3,9 +3,15 @@ session_start();
 
 if (isset($_POST['otp']) && isset($_POST['email'])) {
     $user_otp = trim($_POST['otp']);
-    $email = trim($_POST['email']);
+    $email = strtolower(trim($_POST['email']));
 
-    if (isset($_SESSION['otp']) && $_SESSION['otp'] == $user_otp && $_SESSION['otp_email'] == $email) {
+    if (
+        isset($_SESSION['otp'])
+        && $_SESSION['otp'] == $user_otp
+        && isset($_SESSION['otp_email'])
+        && strtolower($_SESSION['otp_email']) == $email
+    ) {
+        $_SESSION['otp_verified'] = true;
         echo "verified";
     } else {
         echo "invalid";
